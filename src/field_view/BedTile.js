ee12fab56bbcfd31e509593ffb34d987d49c8c54
mattbechtel1 <bechma02@gmail.com>
2020-02-05 14:33:35 -0500

    minor styling addition to field view

--- src/field_view/BedTile.js
+++ src/field_view/BedTile.js
@@ -5,11 +5,16 @@ import { connect } from 'react-redux'
 import { setBed } from '../redux_files/actions'
 import { convertBedToCurrentStage } from '../helpers/conversions'
 
-const BedTile = ({bed, date, setBed}) => <div onClick={() => setBed(bed, date)} className='extend-bottom'>
-    <Image src={dirt} className='rounded-corner' />
-    <h2 className='field-text'>{convertBedToCurrentStage(bed, date).tempCrop || 'No Current Crop'}</h2>
-    <h2 className='field-text'>Stage: {convertBedToCurrentStage(bed, date).status}</h2>
-</div>
+const BedTile = ({bed, date, setBed}) => {
+    const stage = convertBedToCurrentStage(bed, date)
+
+    return <div onClick={() => setBed(bed, date)} className='extend-bottom'>
+        <Image src={dirt} className='rounded-corner' />
+        <h2 className='field-text'>{bed.name}
+        <br />{stage.tempCrop ? stage.tempCrop : 'No Crop Set'}
+        <br />Stage: {convertBedToCurrentStage(bed, date).status}</h2>
+    </div>
+}
 
 const mapStateToProps = ({date}) => ({date})
 
